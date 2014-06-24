e4cb8e5c7fb20561bac9219215f3319bb38fddc1
T107 <T107@PC242>
2014-06-23 19:47:19 -0500

    excepciones

---
+++ practica5/src/Excepciones/Excepciones1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Excepciones;
+
+/**
+ *
+ * @author T107
+ */
+public class Excepciones1 {
+    
+    public static void main (String args[]){
+        int x =8;
+        float y=0;
+        System.out.println("vamos hacer una divicion mala");
+        System.out.println(x/y);
+        System.out.println("si no ocurre la excepcion si aparazco");
+    }
+}
